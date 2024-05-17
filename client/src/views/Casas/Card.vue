fffa024bded67f4e65fb254a14e212679c81722e
CarlosLagos29 <137452483+CarlosLagos29@users.noreply.github.com>
2024-05-17 15:21:56 -0300

    mini correccion

--- client/src/views/Casas/Card.vue
+++ client/src/views/Casas/Card.vue
@@ -5,50 +5,54 @@ import { RouterLink } from 'vue-router';
 
 
 const props = defineProps({
-    casa: Object,
-    order: Number
+  casa: Object,
+  order: Number
 })
 </script>
 
 <template>
-    <div class="bg-white shadow-md rounded-lg p-6 mb-8 flex flex-col-reverse md:flex-row" :class="{'  md:flex-row-reverse': order % 2 === 0}">
-      <div class="flex-grow">
-        <h2 class="text-xl font-bold mb-2">{{ casa.nameModel }}</h2>
-        <div class="grid grid-cols-2 gap-4">
-          <div>
-            <p class="text-gray-600">Rooms:</p>
-            <p>{{ casa.rooms }}</p>
-          </div>
-          <div>
-            <p class="text-gray-600">Bathroom:</p>
-            <p>{{ casa.bathroom }}</p>
-          </div>
-          <div>
-            <p class="text-gray-600">Dimensions:</p>
-            <p>{{ casa.dimensions }}</p>
-          </div>
+  <div class="bg-white shadow-md rounded-lg p-6 mb-8 flex flex-col-reverse md:flex-row"
+    :class="{ '  md:flex-row-reverse': order % 2 === 0 }">
+    <div class="flex-grow">
+      <h2 class="text-xl font-bold mb-2">{{ casa.nameModel }}</h2>
+      <div class="grid grid-cols-2 gap-4">
+        <div>
+          <p class="text-gray-600">Rooms:</p>
+          <p>{{ casa.rooms }}</p>
         </div>
-        <div class="flex mt-4 space-x-4">
-          <button class="bg-buttons text-white px-6 py-3 rounded-md hover:bg-purple-700 transition duration-300">Button 1</button>
-          <button class="bg-buttons text-white px-6 py-3 rounded-md hover:bg-purple-700 transition duration-300">Button 2</button>
-
- <RouterLink :to="`/detail/${casa._id}`">
-          <button class="bg-buttons text-white px-6 py-3 rounded-md hover:bg-purple-700 transition duration-300">Detail</button>
-          <DeleteButton :id="casa._id" :type="'casas'" :name="casa.nameModel"/>
-          <EditButton :id="casa._id" :type="'casas'"/>
-                    </RouterLink>
-
+        <div>
+          <p class="text-gray-600">Bathroom:</p>
+          <p>{{ casa.bathroom }}</p>
         </div>
+        <div>
+          <p class="text-gray-600">Dimensions:</p>
+          <p>{{ casa.dimensions }}</p>
+        </div>
+      </div>
+      <div class="flex mt-4 space-x-4">
+        <button class="bg-buttons text-white px-6 py-3 rounded-md hover:bg-purple-700 transition duration-300">Button
+          1</button>
+        <button class="bg-buttons text-white px-6 py-3 rounded-md hover:bg-purple-700 transition duration-300">Button
+          2</button>
+
+        <RouterLink :to="`/detail/${casa._id}`">
+          <button
+            class="bg-buttons text-white px-6 py-3 rounded-md hover:bg-purple-700 transition duration-300">Detail</button>
+        </RouterLink>
+        <DeleteButton :id="casa._id" :type="'casas'" :name="casa.nameModel" />
+        <EditButton :id="casa._id" :type="'casas'" />
 
       </div>
-      <div>
-        <img :src="casa.offside[0]" :alt="casa.nameModel" class="w-80 h-80 object-cover rounded-md" />
-      </div>
+
     </div>
-  </template>
-  
-  <style scoped>
-  .flex-row-reverse {
-      flex-direction: row-reverse;
-  }
-  </style>
+    <div>
+      <img :src="casa.offside[0]" :alt="casa.nameModel" class="w-80 h-80 object-cover rounded-md" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.flex-row-reverse {
+  flex-direction: row-reverse;
+}
+</style>
