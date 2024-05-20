422ef14ceb217f673c7244780db65e24c0d58b9c
Matias Reyna <matiireyna@hotmail.com>
2024-05-20 18:06:09 -0300

    Cambiar la vista en Home

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,11 +1,11 @@
 <script setup>
-// import Casas from './Casas/Casas.vue';
-import Hero from './HomePage/hero/Hero.vue';
+import Casas from './Casas/Casas.vue';
+// import Hero from './HomePage/hero/Hero.vue';
 </script>
 
 <template>
   <main>
-    <Hero />
-    <!-- <Casas /> -->
+    <!-- <Hero /> -->
+    <Casas />
   </main>
 </template>
