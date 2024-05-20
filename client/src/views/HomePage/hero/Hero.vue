1f81f82b3b182e44a6889f5f17e172fdaaad96bb
Matias Reyna <matiireyna@hotmail.com>
2024-05-20 18:05:05 -0300

    HomePage: in process

---
+++ client/src/views/HomePage/hero/Hero.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { HomeOutlined } from '@ant-design/icons-vue';
+</script>
+
+<template>
+    <div class="w-full min-h-screen bg-cover bg-center text-[#fffcf2] px-[10%] flex items-center justify-center" style="background-image: linear-gradient(rgba(28, 25, 49, 0.7), rgba(28, 25, 49, 0.7)), url('/nexa-hero.webp')">
+      <div class="text-center max-w-2xl">
+        <h2 class="text-5xl font-semibold text-[#ff9f1c] opacity-0 transform -translate-y-12 animate-fadeInUp">Queremos verte crecer</h2>
+        <p class="max-w-2xl mx-auto my-4 leading-7 opacity-0 transform translate-y-12 animate-fadeInUp animation-delay-500">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi provident delectus autem a omnis saepe dolorem consequatur labore. Nobis doloremque illum distinctio facilis aperiam iusto est obcaecati voluptatum nostrum animi!
+        </p>
+  
+        <router-link to="/calendar">
+          <button class="bg-[#fffcf2] text-[#212121] px-6 py-3 text-base rounded-full cursor-pointer border-0 outline-0 inline-flex items-center justify-center transition duration-300 hover:bg-[#ff9f1c] hover:text-white" aria-label="Sacar turno">
+            Mira nuestras casas <HomeOutlined class="w-5 ml-2" />
+          </button>
+        </router-link>
+      </div>
+    </div>
+</template>
+
+<style>
+@keyframes fadeInUp {
+  from {
+    opacity: 0;
+    transform: translateY(-50px);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
+}
+
+.animate-fadeInUp {
+  animation: fadeInUp 1s forwards;
+}
+
+.animation-delay-500 {
+  animation-delay: 0.5s;
+}
+</style>
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
-import Casas from './Casas/Casas.vue';
+// import Casas from './Casas/Casas.vue';
+import Hero from './HomePage/hero/Hero.vue';
 </script>
 
 <template>
   <main>
-    <Casas />
+    <Hero />
+    <!-- <Casas /> -->
   </main>
 </template>
