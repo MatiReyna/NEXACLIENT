f98a685f8d4c5eee6c6928a157d65e8fce5a880d
Matias Reyna <matiireyna@hotmail.com>
2024-05-23 15:01:43 -0300

    Mejoras y estilos en HomePage

---
+++ client/src/views/HomePage/components/Title.vue
@@ -0,0 +1,13 @@
+<script setup>
+const props = defineProps({
+    subTitle: String,
+    title: String
+})
+</script>
+
+<template>
+    <div class="text-center text-yellow-400 text-xs font-semibold uppercase my-7">
+        <p>{{ subTitle }}</p>
+        <h2 class="text-2xl mt-1 text-gray-700">{{ title }}</h2>
+    </div>
+</template>
--- client/src/views/NavBar/NavBar.vue
+++ client/src/views/NavBar/NavBar.vue
@@ -1,9 +1,16 @@
 <script setup>
-import { RouterLink } from 'vue-router';
-import { ref, onMounted, onUnmounted } from 'vue';
+import { RouterLink, useRoute } from 'vue-router';
+import { ref, onMounted, onUnmounted, watch  } from 'vue';
 
 const userLoggedIn = ref(false);
 const isSticky = ref(false);
+
+const route = useRoute();
+const isActiveRoute = ref(false);
+
+const checkActiveRoute = () => {
+  isActiveRoute.value = route.path === '/casas' || route.path === '/admin'
+}
 
 onMounted(() => {
     userLoggedIn.value = sessionStorage.getItem('user') || false
@@ -14,6 +21,11 @@ onMounted(() => {
     }
 
     window.addEventListener('scroll', handleScroll);
+    checkActiveRoute();
+
+    watch(route, () => {
+      checkActiveRoute();
+    });
 
     onUnmounted(() => {
       window.removeEventListener('scroll', handleScroll);
@@ -22,9 +34,9 @@ onMounted(() => {
 </script>
 
 <template>
-  <nav :class="['w-full fixed top-0 left-0 flex items-center justify-between px-10 py-2 text-white shadow-lg z-10 transition duration-300', isSticky ? 'bg-[#3d348b]' : 'bg-transparent']">
+  <nav :class="['w-full fixed top-0 left-0 flex items-center justify-between px-10 py-2 text-white shadow-lg z-10 transition duration-300', isSticky || isActiveRoute ? 'bg-[#3d348b]' : 'bg-transparent']">
     <RouterLink to="/">
-      <img src="/stock-4.webp" alt="LogoHeader" class="w-12 h-auto rounded-full cursor-pointer transition-transform duration-300 transform hover:scale-110" />
+      <img src="/stock-4.webp" alt="LogoHeader" class="w-12 h-12 mb-2 mx-auto md:mx-0 rounded-full cursor-pointer transition-transform transform hover:scale-110" />
     </RouterLink>
     <ul class="flex space-x-6">
       <li>
@@ -38,9 +50,9 @@ onMounted(() => {
         </RouterLink>
       </li>
       <li>
-        <RouterLink to="/about" class="transition-colors duration-300 hover:text-[#7678ed]">
+        <a href="#about" class="transition-colors duration-300 hover:text-[#7678ed]">
           Sobre Nosotros
-        </RouterLink>
+        </a>
       </li>
       <li>
         <RouterLink v-if="userLoggedIn" to="/admin" class="transition-colors duration-300 hover:text-[#7678ed]">
