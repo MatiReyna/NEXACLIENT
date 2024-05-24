1fdb42bb1c61388b885367377859e0075beeddc1
Matias Reyna <matiireyna@hotmail.com>
2024-05-24 15:47:09 -0300

    Mejoras en HomePage

--- client/src/views/HomePage/components/Sponsors.vue
+++ client/src/views/HomePage/components/Sponsors.vue
@@ -2,18 +2,26 @@
 </script>
 
 <template>
-    <div class="overflow-hidden relative h-40">
+  <div class="overflow-hidden relative h-40">
       <div class="flex animate-scroll">
-        <img src="/stock-4.webp" alt="Brand 1" class="w-60 h-32 object-contain mx-4 rounded-lg shadow-md" />
-        <img src="/stock-4.webp" alt="Brand 2" class="w-60 h-32 object-contain mx-4 rounded-lg shadow-md" />
-        <img src="/stock-4.webp" alt="Brand 3" class="w-60 h-32 object-contain mx-4 rounded-lg shadow-md" />
-        <img src="/stock-4.webp" alt="Brand 4" class="w-60 h-32 object-contain mx-4 rounded-lg shadow-md" />
-        <img src="/stock-4.webp" alt="Brand 1" class="w-60 h-32 object-contain mx-4 rounded-lg shadow-md" />
-        <img src="/stock-4.webp" alt="Brand 2" class="w-60 h-32 object-contain mx-4 rounded-lg shadow-md" />
-        <img src="/stock-4.webp" alt="Brand 3" class="w-60 h-32 object-contain mx-4 rounded-lg shadow-md" />
-        <img src="/stock-4.webp" alt="Brand 4" class="w-60 h-32 object-contain mx-4 rounded-lg shadow-md" />
+          <img src="/belgrano.png" alt="Brand 1" class="w-64 h-32 object-cover mx-4 rounded-md filter grayscale" />
+          <img src="/coca.png" alt="Brand 2" class="w-64 h-32 object-cover mx-4 rounded-md filter grayscale" />
+          <img src="/pepsi.png" alt="Brand 3" class="w-64 h-32 object-cover mx-4 rounded-md filter grayscale" />
+          <img src="/sprite.png" alt="Brand 4" class="w-64 h-32 object-cover mx-4 rounded-md filter grayscale" />
+          <img src="/belgrano.png" alt="Brand 1" class="w-64 h-32 object-cover mx-4 rounded-md filter grayscale" />
+          <img src="/coca.png" alt="Brand 2" class="w-64 h-32 object-cover mx-4 rounded-md filter grayscale" />
+          <img src="/pepsi.png" alt="Brand 3" class="w-64 h-32 object-cover mx-4 rounded-md filter grayscale" />
+          <img src="/sprite.png" alt="Brand 4" class="w-64 h-32 object-cover mx-4 rounded-md filter grayscale" />
+          <img src="/belgrano.png" alt="Brand 1" class="w-64 h-32 object-cover mx-4 rounded-md filter grayscale" />
+          <img src="/coca.png" alt="Brand 2" class="w-64 h-32 object-cover mx-4 rounded-md filter grayscale" />
+          <img src="/pepsi.png" alt="Brand 3" class="w-64 h-32 object-cover mx-4 rounded-md filter grayscale" />
+          <img src="/sprite.png" alt="Brand 4" class="w-64 h-32 object-cover mx-4 rounded-md filter grayscale" />
+          <img src="/belgrano.png" alt="Brand 1" class="w-64 h-32 object-cover mx-4 rounded-md filter grayscale" />
+          <img src="/coca.png" alt="Brand 2" class="w-64 h-32 object-cover mx-4 rounded-md filter grayscale" />
+          <img src="/pepsi.png" alt="Brand 3" class="w-64 h-32 object-cover mx-4 rounded-md filter grayscale" />
+          <img src="/sprite.png" alt="Brand 4" class="w-64 h-32 object-cover mx-4 rounded-md filter grayscale" />
       </div>
-    </div>
+  </div>
 </template>
 
 <style scoped>
@@ -30,4 +38,8 @@
   display: flex;
   animation: scroll 20s linear infinite;
 }
+
+/* .filter {
+    filter: grayscale(80%);
+} */
 </style>
