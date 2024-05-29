efed4d9e8cfe27f69e24f38aa609013167166a19
CarlosLagos29 <137452483+CarlosLagos29@users.noreply.github.com>
2024-05-29 15:08:53 -0300

    detalles del login en estilos

--- client/src/views/Admin/LogIn/LogIn.vue
+++ client/src/views/Admin/LogIn/LogIn.vue
@@ -30,19 +30,22 @@ const handleLogin = async () => {
 </script>
 
 <template>
-  <form class=" grid items-center justify-center gap-y-2 m-3" @submit.prevent="handleLogin">
+<div class="flex items-center justify-center h-[50vh]">
+  <form class="w-96 grid gap-y-2 p-6 bg-white rounded-lg shadow-custom" @submit.prevent="handleLogin">
+    
     <label>Email:</label>
-    <input class=" border-2 border-slate-500 rounded-md px-2 placeholder:italic" :class="{ ' border-red-400': errors.email }" type="text"
+    <input class="border-2 border-buttons rounded-md px-2 placeholder-italic" :class="{ 'border-red-400': errors.email }" type="text"
       name="email" placeholder="ejemplo@gmail.com" v-model="session.email">
-    <a-alert class=" rounded-md py-1 text-red-600 " v-if="errors.email" type="error" :message="errors.email" banner />
+    <a-alert class="rounded-md py-1 text-red-600" v-if="errors.email" type="error" :message="errors.email" banner />
 
     <label>Contraseña:</label>
-    <input class=" border-2 border-slate-500 rounded-md px-2 placeholder:italic" :class="{ '  border-red-400': errors.password }"
+    <input class="border-2 border-buttons rounded-md px-2 placeholder-italic" :class="{ 'border-red-400': errors.password }"
       type="password" name="password" placeholder="contraseña..." v-model="session.password">
-    <a-alert class=" rounded-md py-1 text-red-600 " v-if="errors.password" type="error" :message="errors.password" banner />
+    <a-alert class="rounded-md py-1 text-red-600" v-if="errors.password" type="error" :message="errors.password" banner />
 
-    <a-button class=" bg-buttons" type="primary" @click="handleLogin">LogIn</a-button>
+    <a-button class="bg-buttons text-white" type="primary" @click="handleLogin">LogIn</a-button>
     
   </form>
+</div>
 
 </template>
