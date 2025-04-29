088ea4ce6e812d2923f6f6f5452237a53355695d
Mati Reyna <matiireyna@hotmail.com>
2025-04-28 21:33:44 -0300

    Posible solucion a lo visual

--- client/src/components/ui/tabs.jsx
+++ client/src/components/ui/tabs.jsx
@@ -33,8 +33,8 @@ const TabsTrigger = forwardRef(({ className, value, ...props }, ref) => {
             ref={ ref }
             className={ cn("inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 aria-selected",
                 isActive
-                    ? "bg-[#592e83] text-white"
-                    : "hover:bg-[#592e83]/20 hover:text-[#592e83]",
+                    ? "bg-muted text-muted-foreground"
+                    : "hover:bg-muted/80",
                 className
             ) }
             onClick={ () => context.setValue(value) } { ...props }
