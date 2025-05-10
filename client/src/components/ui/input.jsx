d8ed38e58949596e71a2778c797c0f8c42355e7f
Mati Reyna <matiireyna@hotmail.com>
2025-05-10 18:55:26 -0300

    Mejoras en archivo input

--- client/src/components/ui/input.jsx
+++ client/src/components/ui/input.jsx
@@ -7,7 +7,7 @@ const Input = forwardRef(({ className, type = 'text', error = false, ...props },
             type={ type }
             ref={ ref }
             className={ cn(
-                "flex h-10 w-full rounded-xl border bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+                "flex h-11 w-full rounded-xl border bg-background px-4 py-2 text-base ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
                 error ? "border-red-500 focus-visible:ring-red-500" : "border-input focus-visible:ring-ring",
                 className
             ) }
